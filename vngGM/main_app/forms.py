104f488b1b47796e443ff2e649a8ebca2b88ee50
lucagalbu <luca.galbusera@unibas.ch>
2019-10-04 09:15:56 +0200

    Fixed default values for the form AddSchedule. Now the new speaker and the date are automatically updated

--- vngGM/main_app/forms.py
+++ vngGM/main_app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, ValidationError, Email
 from wtforms.fields.html5 import DateField
 from main_app.models import Users, Papers, Schedules
 from flask import flash
-from datetime import timedelta
+#from datetime import timedelta
 from sqlalchemy import and_, or_, desc
 
 class AddUser(FlaskForm):
@@ -26,18 +26,18 @@ class AddUser(FlaskForm):
 
 class AddSchedule(FlaskForm):
 	#Find the next user (Look at user list and select the ones with the next id)
-	current_user_id = Users.query.join(Schedules).order_by(desc('date')).first().id
-	if current_user_id == Users.query.order_by(desc('id')).first().id:
-		current_user_id == Users.query.order_by('id').first().id
-	next_user = Users.query.order_by(Users.id).filter(Users.id > current_user_id).first()
+	#current_user_id = Users.query.join(Schedules).order_by(desc('date')).first().id
+	#if current_user_id == Users.query.order_by(desc('id')).first().id:
+#		current_user_id == Users.query.order_by('id').first().id
+	#next_user = Users.query.order_by(Users.id).filter(Users.id > current_user_id).first()
 	
 
 	#Find the next Thursday
-	current_day = Schedules.query.order_by(desc('date')).first().date
-	next_thursday = current_day + timedelta(7) if current_day.weekday() == 3 else current_day + timedelta((3-current_day.weekday()) % 7 )
+	#current_day = Schedules.query.order_by(desc('date')).first().date
+	#next_thursday = current_day + timedelta(7) if current_day.weekday() == 3 else current_day + timedelta((3-current_day.weekday()) % 7 )
 	
-	speaker = StringField('Speaker', validators=[DataRequired()], default = next_user.name)
-	date = DateField('Date', validators=[DataRequired()], default = next_thursday)
+	speaker = StringField('Speaker', validators=[DataRequired()])#, default = next_user.name)
+	date = DateField('Date', validators=[DataRequired()])#, default = next_thursday)
 	paper_title = StringField('Paper')
 	paper_doi = StringField('Paper doi')
 	paper_url = StringField('Paper url')
--- vngGM/main_app/routes.py
+++ vngGM/main_app/routes.py
@@ -4,6 +4,8 @@ from flask import render_template, flash, redirect, url_for
 from main_app.models import Users, Papers, Schedules
 from sqlalchemy.orm import aliased
 from sqlalchemy import desc
+from datetime import timedelta, datetime
+
 
 @app.route('/')
 @app.route('/index')
@@ -43,6 +45,7 @@ def listUsers():
 def addSchedule():
 	form = AddSchedule()
 
+
 	if form.validate_on_submit():
 		if form.paper_title.data is not "":
 			user=Users.query.filter_by(name=form.paper_proposer.data).first()
@@ -59,5 +62,30 @@ def addSchedule():
 
 		db.session.commit()
 		return redirect(url_for('addSchedule'))
+	else:
+		#Find the next user (Look at user list and select the ones with the next id)
+		current_user = Users.query.join(Schedules).order_by(desc('date')).first()
+		
+		if(current_user is None): #This happens if the schedule is empty
+			current_user_id = Users.query.order_by('id').first().id
+		else:
+			current_user_id = current_user.id
+		#If we have reached the bottom of the user list, start from the top again
+		if current_user_id == Users.query.order_by(desc('id')).first().id:
+			current_user_id == Users.query.order_by('id').first().id
+		next_user = Users.query.order_by(Users.id).filter(Users.id > current_user_id).first()
+			
+		#Find the next Thursday
+		current_day = Schedules.query.order_by(desc('date')).first()
+		if current_day is None: #This happens if the schedule is empty
+			current_day = datetime.now()
+		else:
+			current_day = current_day.date
+		next_thursday = current_day + timedelta(7) if current_day.weekday() == 3 else current_day + timedelta((3-current_day.weekday()) % 7 )
+
+		#Set the default user and date
+		form.date.default=next_thursday
+		form.speaker.default=next_user.name
+		form.process()
 
 	return render_template('addSchedule.html', form = form)
